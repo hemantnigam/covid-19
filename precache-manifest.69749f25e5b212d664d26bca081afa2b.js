3b6743f18c3e24bc2ce6d8e26592f00354ca60f4
Hemant Nigam <hemant.nigam@nagarro.com>
2020-04-12 13:56:17 +0530

    added dist folder

---
+++ precache-manifest.69749f25e5b212d664d26bca081afa2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f200cb96faf7999181",
+    "url": "/covid-19/css/app.84b1e486.css"
+  },
+  {
+    "revision": "b2892ca84c20d83fbe7d",
+    "url": "/covid-19/css/chunk-vendors.d889c800.css"
+  },
+  {
+    "revision": "1b5de2e903a1d6461b4997e096d1e4f8",
+    "url": "/covid-19/img/globe.1b5de2e9.png"
+  },
+  {
+    "revision": "639236a9e2d910aaf282f7fd66e49ca4",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "61f200cb96faf7999181",
+    "url": "/covid-19/js/app.cf4f5515.js"
+  },
+  {
+    "revision": "b2892ca84c20d83fbe7d",
+    "url": "/covid-19/js/chunk-vendors.7d5a8c9d.js"
+  },
+  {
+    "revision": "ec04c6024736895c0418a80e8e32ced2",
+    "url": "/covid-19/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-19/robots.txt"
+  }
+]);
