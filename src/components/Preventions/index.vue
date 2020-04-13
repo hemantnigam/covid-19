5a066d947641559679c76269e5b21bc2190ceeef
Hemant Nigam <hemant.nigam@nagarro.com>
2020-04-13 17:57:57 +0530

    added accordion

--- src/components/Preventions/index.vue
+++ src/components/Preventions/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      
+  <div class="ml-4 mt-4 mr-4 mb-4">
+    <h1>Coronavirus Precautions</h1>
+    <ul>
+      <li v-for="(prevention,i) in preventions" :key="i">
+          {{prevention}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name:"Preventions"
-}
+  name: "Preventions",
+  data() {
+    return {
+      preventions: [
+        "Regular hand washing",
+        "Covering mouth and nose when coughing and sneezing",
+        "If you are taking care of someone who is sick, try to stay 6 feet away – this is the distance virus – containing droplets can travel through a sneeze or cough",
+        "Avoid close contact with anyone showing symptoms of respiratory illness as coughing and sneezing",
+        "Avoid touching your eyes, nose, and mouth",
+        "Stay home when you are sick",
+        "Clean and disinfect frequently touched objects and surfaces using a regular objects and surfaces using a regular household cleaning spray or wipe."
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/SearchTab/index.vue
+++ src/components/SearchTab/index.vue
@@ -1,13 +1,51 @@
 <template>
-  <div></div>
+  <v-expansion-panels focusable tile>
+    <v-expansion-panel v-for="(country,i) in allCountriesData" :key="i">
+      <v-expansion-panel-header>
+          <div>{{country.country}}</div>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <div class="mt-4 mb-4">
+          <div class="d-flex justify-space-around">
+            <div>Confirmed: {{country.stats.confirmed}}</div>
+            <div>Deaths: {{country.stats.deaths}}</div>
+          </div>
+          <div class="d-flex justify-center">
+            <div>Recovered: {{country.stats.deaths}}</div>
+          </div>
+        </div>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
 export default {
-    name:"SearchTab"
-}
+  name: "SearchTab",
+  data() {
+    return {
+      allCountriesData: []
+    };
+  },
+  created() {
+    this.getAllCountriesData();
+  },
+  methods: {
+    getAllCountriesData: function() {
+      fetch("https://corona.lmao.ninja/v2/jhucsse")
+        .then(response => response.json())
+        .then(data => {
+          this.allCountriesData = data;
+          console.log(this.allCountriesData);
+        });
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.v-expansion-panel-header--active {
+  background-color: #113a5d !important;
+  color: #dae1e7 !important;
+}
 </style>
