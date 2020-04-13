f5bc186e255f24e4bf5f33063591ca2517b5d476
Hemant Nigam <hemant.nigam@nagarro.com>
2020-04-13 10:59:39 +0530

    Updates

--- precache-manifest.153940b6b18d4bccb20bf01596986d22.js
+++ precache-manifest.153940b6b18d4bccb20bf01596986d22.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d81383695e7ce4922c8",
-    "url": "/covid-19/css/app.84b1e486.css"
+    "revision": "9d0c9669bc2202dc6f14",
+    "url": "/covid-19/css/app.d90225e3.css"
   },
   {
     "revision": "b2892ca84c20d83fbe7d",
@@ -12,19 +12,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-19/img/globe.1b5de2e9.png"
   },
   {
-    "revision": "2021f36500b899b9d1229a1ce5a3e7a7",
+    "revision": "ffdeeb235a0b07a25b02db41a6cc7367",
     "url": "/covid-19/index.html"
   },
   {
-    "revision": "9d81383695e7ce4922c8",
-    "url": "/covid-19/js/app.50fc2b9a.js"
+    "revision": "9d0c9669bc2202dc6f14",
+    "url": "/covid-19/js/app.e83a1ca2.js"
   },
   {
     "revision": "b2892ca84c20d83fbe7d",
     "url": "/covid-19/js/chunk-vendors.7d5a8c9d.js"
   },
   {
-    "revision": "ec04c6024736895c0418a80e8e32ced2",
+    "revision": "2cb56af3e848855230c586a57a195ee4",
     "url": "/covid-19/manifest.json"
   },
   {
