9eaa46fc3bc3fa39bed8d072e98643de1fdf8db0
Hemant Nigam <hemant.nigam@nagarro.com>
2020-04-13 10:54:46 +0530

    configure status bar

---
+++ dist/precache-manifest.a902abd45d812e0c5933aa7e93f2bf63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a5450d674d401c36fe",
+    "url": "/covid-19/css/app.a080e606.css"
+  },
+  {
+    "revision": "f57f144c6e092306d5e2",
+    "url": "/covid-19/css/chunk-vendors.fb20704c.css"
+  },
+  {
+    "revision": "1b5de2e903a1d6461b4997e096d1e4f8",
+    "url": "/covid-19/img/globe.1b5de2e9.png"
+  },
+  {
+    "revision": "dc741debc6d65d127b97f26d70e772d6",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d3a5450d674d401c36fe",
+    "url": "/covid-19/js/app.7b79c85a.js"
+  },
+  {
+    "revision": "f57f144c6e092306d5e2",
+    "url": "/covid-19/js/chunk-vendors.99564f14.js"
+  },
+  {
+    "revision": "2cb56af3e848855230c586a57a195ee4",
+    "url": "/covid-19/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-19/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  pwa: {
+    themeColor: '#113a5d'
+  },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/covid-19/'
     : '/'
